fe64890a529a3285879c3d90b1a477e8c6b8620d
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-02-25 14:00:51 -0800

    questions started

---
+++ app.js
@@ -0,0 +1,43 @@
+const allQuestions = [
+{
+    question: "Tupperware", 
+    answers: {
+        a: 'Tup-Uh-Wair',
+        b: 'Tup-er-wair', 
+        c: 'Tub-er-wair', 
+        d: 'Two-puh-wair'
+    },
+    correctAnswer: 'a'
+},
+{
+    question: "Colonel",
+    answers: {
+        a: 'Core-low-nel',
+        b: 'Call-Oh-nel',
+        c: 'kur-nl',
+        d: 'Coal-oh-nel'
+    },
+    correctAnswer: 'c'
+},
+{
+    question: "Dachshund",
+    answers: {
+        a: 'dacks-ind',
+        b: 'Dah-Chs-Hund',
+        c: 'Docks-how-nd',
+        d: 'dahks-hund'
+    },
+    correctAnswer: 'd'
+},
+{
+    question: "Renaissance",
+    answers: {
+        a: 'Ri-Nay-Suhns',
+        b: 'Ri-Nay-Sahns',
+        c: 'Ren-ih-sahnz',
+        d: 'Rena-is-ahnz',
+    },
+    correctAnswer: 'a'
+},
+     
+]
