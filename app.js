360e9f016eb2cc1465ae4da4be3b0912803d6c63
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-03-02 17:19:40 -0800

    revised javascript and HTML names

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const answer1 = document.getElementById('a');
+const answer2 = document.getElementById('b');
+const answer3 = document.getElementById('c');
+const answer4 = document.getElementById('d');
+const questionCount = document.querySelector('#questionCount');
+const questions = document.querySelector('#questions');
+const score = document.querySelector('#score');
+
+const lastQuestion = myQuestions.length - 1;
+
 var myQuestions = [
 {
     question: "Tupperware", 
@@ -100,70 +110,3 @@ var myQuestions = [
     correctAnswer: 'c'
 },
 ]   
-
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-// generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-//   function showQuestions(questions, quizContainer){
-    function showQuestions(questions) {
-        let questionIndex = 0;
-      
-        function showNextQuestion() {
-          const questionContainer = document.getElementById("question-container");
-          questionContainer.innerHTML = "";
-      
-          if (questionIndex >= questions.length) {
-            questionContainer.innerText = "No more questions!";
-            return;
-          }
-      
-          const currentQuestion = questions[questionIndex];
-          const questionText = document.createElement("h2");
-          questionText.innerText = currentQuestion.question;
-          questionContainer.appendChild(questionText);
-      
-          currentQuestion.options.forEach((option) => {
-            const optionLabel = document.createElement("label");
-            const optionInput = document.createElement("input");
-            optionInput.setAttribute("type", "radio");
-            optionInput.setAttribute("name", "answer");
-            optionInput.setAttribute("value", option);
-            optionLabel.appendChild(optionInput);
-            optionLabel.append(` ${option}`);
-            questionContainer.appendChild(optionLabel);
-          });
-      
-          const submitButton = document.createElement("button");
-          submitButton.innerText = "Submit";
-          submitButton.addEventListener("click", () => {
-            const selectedOption = document.querySelector('input[name="answer"]:checked');
-            if (!selectedOption) {
-              alert("Please select an answer");
-              return;
-            }
-      
-            const answer = selectedOption.value;
-            if (answer === currentQuestion.answer) {
-              alert("Correct!");
-            } else {
-              alert(`Incorrect. The correct answer is ${currentQuestion.answer}`);
-            }
-      
-            questionIndex++;
-            showNextQuestion();
-          });
-      
-          questionContainer.appendChild(submitButton);
-        }
-      
-        showNextQuestion();
-      }
-//     }
-// }
-
-      showQuestions(questions);
